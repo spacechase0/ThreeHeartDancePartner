292fd1c493bee62905d88751b916ac6854f55746
Fynzou <35840637+Fynzou@users.noreply.github.com>
2018-05-31 17:40:45 -0700

    Add files via upload
    Config File for Lower Hearts.

---
+++ ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeHeartDancePartner
+{
+    class ModConfig
+    {
+        public int NumberOfHeartsRequiredForAcceptance { get; set; } = 3;
+    }
+}
